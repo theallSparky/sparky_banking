69e2888149a70fd77e049b63df8bbbf4fa324812
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-08-18 18:27:25 -0500

    Made AnimatedCounter and worked on TotalBalanceBox

---
+++ components/DoughnutChart.tsx
@@ -0,0 +1,8 @@
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+
+const DoughnutChart = () => {
+  return <div>DoughnutChart</div>;
+};
+
+export default DoughnutChart;
