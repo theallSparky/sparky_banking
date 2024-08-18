a93a485ab71bcc3cf87c2560b1fe046b1f3eb507
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-08-18 14:43:28 -0500

    Create HeaderBox and TotalBalanceBox components. Add home page Header box and total balance box

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,23 @@
 import HeaderBox from "@/components/HeaderBox";
-import React from "react";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
+  const loggedIn = { firstName: "Harrison" };
   return (
     <section className="home">
       <div className="home-content">
         <div className="home-header">
-          <HeaderBox />
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1999.99}
+          />
         </div>
       </div>
     </section>
